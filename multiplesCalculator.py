e2254fa4c82a6512facf70c2f52d5a86c9558c27
Nehel Sunny <69785913+nehel-sk@users.noreply.github.com>
2022-06-11 11:03:44 +0530

    Add files via upload

---
+++ multiplesCalculator.py
@@ -0,0 +1,19 @@
+k=1
+while(k<=70):
+    print("#",end="")
+    k=k+1
+print("\n")
+print("\t\tWELCOME TO MULTIPLER CALCULATOR")
+s=int(input("enter the limit for the sequence:"))
+i=int(input("enter the starting point:"))
+n=int(input("enter the number who's multiples are to be printed:"))
+print("the numbers are:")
+while(i<=s):
+    if(i%n==0):
+        print(i)
+    i=i+1
+print("\t\tThank You For Using This Program")
+k=1
+while(k<=70):
+    print("#",end="")
+    k=k+1
