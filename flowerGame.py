465b3aec619c5099eeaabd20d3406b85fa3f1ebb
Nehel Sunny <69785913+nehel-sk@users.noreply.github.com>
2022-06-12 13:06:16 +0530

    Add files via upload

---
+++ flowerGame.py
@@ -0,0 +1,17 @@
+import random
+
+flowers=["lilly","rose","jasmine","lotus"]
+print("list is:",flowers)
+
+selection=str(input("Enter your selection:"))
+comp_guess=random.randrange(0,3)
+guess=flowers[comp_guess]
+if selection==guess:
+    print("you won")
+   
+else:
+    print("you lost")
+print("computer guess:",guess)
+print("your selection:",selection)
+
+
